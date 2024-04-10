0ffdcbff308821d7f23e5d9ad55ffea11871489e
NyXdeity <kiritorsp123456789@gmail.com>
2024-04-10 13:52:13 +0530

    update

--- class1.2.js
+++ class1.2.js
@@ -241,5 +241,20 @@ for(let i=0; i<allUser.length;i++){
 // function greet() {//setTime out function 
 //     console.log("hello, world:");
 // }
-// setTimeout( greet , 3*1000)
+
+function calculateArithmetic(a,b,arithmeticFinalFunction){
+    function arithmeticFinalFunction(a,b){
+        return a+b;
+    }
+
+    const ans = arithmeticFinalFunction(a,b)
+    return ans;
+}
+function sum(a,b){
+    return a+b;
+}
+const value = calculateArithmetic(1,2,sum);
+console.log(value);
+
+setTimeout( greet , 3*1000)
 
---
+++ test.js
@@ -0,0 +1,13 @@
+function calculateArithmetic(a,b,arithmeticFinalFunction){
+    function arithmeticFinalFunction(a,b){
+        return a+b;
+    }
+
+    const ans = arithmeticFinalFunction(a,b)
+    return ans;
+}
+function sum(a,b){
+    return a+b;
+}
+const value = calculateArithmetic(1,2,sum);
+console.log(value);
